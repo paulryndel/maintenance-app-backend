1dff208c236f5662da2f03a418047e4ec08db8b9
paulryndel <paulryndel@akelakiba.com>
2025-09-09 16:01:23 +0700

    Configure Tailwind CSS and build process

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./*.{html,js}", // Scans all .html and .js files in the root
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-dark': '#1a202c',
+        'brand-gray': '#6b7280',
+        'brand-light-gray': '#f7fafc',
+        'brand-blue': '#0064D2',
+        'brand-yellow': '#ffc107',
+      }
+    },
   },
   plugins: [],
 }
